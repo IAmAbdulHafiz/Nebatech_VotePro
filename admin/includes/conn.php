2490abf2e49db4631a091166036a01be9baa6b50
ABDUL-HAFIZ YUSSIF <imabdulhafiz.yussif@gmail.com>
2024-09-19 06:56:17 +0000

    update database connection in conn.php

--- admin/includes/conn.php
+++ admin/includes/conn.php
@@ -1,5 +1,5 @@
 <?php
-	$conn = new mysqli('localhost', 'root', '', 'nebatech_votepro_db');
+	$conn = new mysqli('localhost', 'root', '', 'nebatech_db');
 
 	if ($conn->connect_error) {
 	    die("Connection failed: " . $conn->connect_error);
