1a7cf14d09c89b2ae2b9917e64ba1ae71edf5004
ABDUL-HAFIZ YUSSIF <imabdulhafiz.yussif@gmail.com>
2024-09-19 13:13:23 +0000

    Update Admin index and header file

--- admin/index.php
+++ admin/index.php
@@ -1,70 +1,67 @@
 <?php
-  	session_start();
-  	if(isset($_SESSION['admin'])){
-    	header('location:home.php');
-  	}
+session_start();
+if (isset($_SESSION['admin'])) {
+    header('location:home.php');
+}
 ?>
 
 <!DOCTYPE HTML>
 <html>
-	<head>
-		<title> </title>
-		<link rel="stylesheet" href="../dist/css/style.css">
-		<link rel="stylesheet" href="../bower_components/font-awesome/css/font-awesome.min.css">
-	</head>
-	<?php include 'includes/header.php'; ?>
+<head>
+    <title>Admin Login - eVotePro</title>
+    <link rel="icon" type="image/x-icon" sizes="32x32" href="../images/favicon.png">
+    <link rel="stylesheet" href="../dist/css/style.css">
+    <link rel="stylesheet" href="../bower_components/font-awesome/css/font-awesome.min.css">
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+</head>
 <body>
-<div class="background-overlay">
-	<div class="login-box-left">
-		<div class="login-head">
-			<b>Nebatech VotePro</b>
-		</div>
-		<div class="login-logo">
-			<img class="login-logo-icon" src="../images/vote_logo.png">
-		</div>
-	</div>
-	<div class="login-box-right">
-		<div class="welcome-text">
-			<b>Welcome back</b>
-		</div>
-
-		<div class="login-box-container" style="margin-top: 50px;">
-			<div class="login-logo">
-				<b>Admin</b>
-			</div>
-			<p class="login-box-msg">Sign in to start your session</p>
-
-			<form action="login.php" method="POST">
-				<div class="form-group has-feedback">
-					<input type="text" class="form-group-user" name="username" placeholder="Username" required>
-					<span class="glyphicon glyphicon-user form-control-feedback"></span>
-				</div>
-			<div class="form-group has-feedback">
-				<input type="password" class="form-group-user" name="password" placeholder="Password" required>
-				<span class="glyphicon glyphicon-lock form-control-feedback"></span>
-			</div>
-				<div class="row">
-					<div class="col-xs-4s">
-						<button type="submit" class=" btn-sign-in btn-block btn-flat" name="login"><i class="fa fa-sign-in"></i> Sign In</button>
-					</div>
-				</div>
-			</form>
-		</div>
-		<?php
-			if(isset($_SESSION['error'])){
-				echo "
-					<div class='callout callout-danger text-center mt20'>
-						<p>".$_SESSION['error']."</p> 
-					</div>
-				";
-				unset($_SESSION['error']);
-			}
-		?>
-	</div>
-	<footer class="login-footer">
-    	<strong>Copyright &copy; 2024 <a href="https://www.nebatech.com">Nebatech Software Solution</a></strong> <b>All rights reserved</b>
-	</footer>
-</div>
-<?php include 'includes/scripts.php' ?>
+    <?php include 'includes/header.php'; ?>
+    <div class="background-overlay">
+        <div class="login-box-left">
+            <div class="login-head">
+                <b>Nebatech VotePro</b>
+            </div>
+            <div class="login-logo">
+                <img class="login-logo-icon" src="../images/vote_logo.png" alt="VotePro Logo">
+            </div>
+        </div>
+        <div class="login-box-right">
+            <div class="welcome-text">
+                <b>Welcome back</b>
+            </div>
+            <div class="login-box-container">
+                <div class="login-logo">
+                    <b>Admin</b>
+                </div>
+                <p class="login-box-msg">Sign in to start your session</p>
+                <form action="login.php" method="POST">
+                    <div class="form-group has-feedback">
+                        <input type="text" class="form-group-user" name="username" placeholder="Username" required>
+                        <span class="glyphicon glyphicon-user form-control-feedback"></span>
+                    </div>
+                    <div class="form-group has-feedback">
+                        <input type="password" class="form-group-user" name="password" placeholder="Password" required>
+                        <span class="glyphicon glyphicon-lock form-control-feedback"></span>
+                    </div>
+                    <button type="submit" class="btn-sign-in btn-block" name="login"><i class="fa fa-sign-in"></i> Sign In</button>
+                </form>
+            </div>
+            <?php
+            if (isset($_SESSION['error'])) {
+                echo "
+                    <div class='callout callout-danger text-center mt20'>
+                        <p>".$_SESSION['error']."</p> 
+                    </div>
+                ";
+                unset($_SESSION['error']);
+            }
+            ?>
+        </div>
+        <footer class="login-footer">
+            <strong>Copyright &copy; 2024 <a href="https://www.nebatech.com">Nebatech Software Solution</a></strong> <b>All rights reserved</b>
+        </footer>
+    </div>
+    <?php include 'includes/scripts.php'; ?>
 </body>
 </html>
--- index.php
+++ index.php
@@ -10,64 +10,62 @@
 ?>
 <!DOCTYPE HTML>
 <html>
-	<head>
-		<title> </title>
-		<link rel="stylesheet" href="dist/css/style.css">
-		<link rel="stylesheet" href="../bower_components/font-awesome/css/font-awesome.min.css">
-	</head>
-	<?php include 'includes/header.php'; ?>
+<head>
+    <title>Nebatech VotePro | Login</title>
+    <link rel="stylesheet" href="dist/css/style.css">
+    <link rel="stylesheet" href="../bower_components/font-awesome/css/font-awesome.min.css">
+</head>
+<?php include 'includes/header.php'; ?>
 <body>
 <div class="background-overlay">
-	<div class="login-box-left">
-		<div class="login-head">
-			<b>Nebatech VotePro</b>
-		</div>
-		<div class="login-logo">
-			<img class="login-logo-icon" src="images/vote_logo.png">
-		</div>
-	</div>
-	<div class="login-box-right">
-		<div class="welcome-text">
-			<b>Welcome back</b>
-		</div>
-
-		<div class="login-box-container" style="margin-top: 50px;">
-			<div class="login-logo">
-				<b>Voter</b>
-			</div>
-			<p class="login-box-msg">Sign in to Vote</p>
-
-			<form action="login.php" method="POST">
-				<div class="form-group has-feedback">
-					<input type="text" class="form-group-user" name="voter" placeholder="Voter's ID" required>
-					<span class="glyphicon glyphicon-user form-control-feedback"></span>
-				</div>
-			<div class="form-group has-feedback">
-				<input type="password" class="form-group-user" name="password" placeholder="Password" required>
-				<span class="glyphicon glyphicon-lock form-control-feedback"></span>
-			</div>
-				<div class="row">
-					<div class="col-xs-4s">
-						<button type="submit" class=" btn-sign-in btn-block" name="login"><i class="fa fa-sign-in"></i> Sign In</button>
-					</div>
-				</div>
-			</form>
-		</div>
-		<?php
-			if(isset($_SESSION['error'])){
-				echo "
-					<div class='callout callout-danger text-center mt20'>
-						<p>".$_SESSION['error']."</p> 
-					</div>
-				";
-				unset($_SESSION['error']);
-			}
-		?>
-	</div>
-	<footer class="login-footer">
-    	<strong>Copyright &copy; 2024 <a href="https://github.com/IAmAbdulHafiz/Nebatech_VotePro">Nebatech VotePro</a></strong> <b>All rights reserved</b>
-	</footer>
+    <div class="login-box-left">
+        <div class="login-head">
+            <b>Nebatech VotePro</b>
+        </div>
+        <div class="login-logo">
+            <img class="login-logo-icon" src="images/vote_logo.png" alt="VotePro Logo">
+        </div>
+    </div>
+    <div class="login-box-right">
+        <div class="welcome-text">
+            <b>Welcome back</b>
+        </div>
+        <div class="login-box-container">
+            <div class="login-logo">
+                <b>Voter</b>
+            </div>
+            <p class="login-box-msg">Sign in to Vote</p>
+            <form action="login.php" method="POST">
+                <div class="form-group has-feedback">
+                    <input type="text" class="form-group-user" name="voter" placeholder="Voter's ID" required>
+                    <span class="glyphicon glyphicon-user form-control-feedback"></span>
+                </div>
+                <div class="form-group has-feedback">
+                    <input type="password" class="form-group-user" name="password" placeholder="Password" required>
+                    <span class="glyphicon glyphicon-lock form-control-feedback"></span>
+                </div>
+                <div class="row">
+                    <div class="col-xs-12">
+                        <button type="submit" class="btn-sign-in btn-block" name="login"><i class="fa fa-sign-in"></i> Sign In</button>
+                    </div>
+                </div>
+            </form>
+        </div>
+        <?php
+            if(isset($_SESSION['error'])){
+                echo "
+                    <div class='callout callout-danger text-center mt20'>
+                        <p>".$_SESSION['error']."</p> 
+                    </div>
+                ";
+                unset($_SESSION['error']);
+            }
+        ?>
+    </div>
+    <footer class="login-footer">
+        <strong>Copyright &copy; 2024 <a href="https://github.com/IAmAbdulHafiz/Nebatech_VotePro">Nebatech VotePro</a></strong> <b>All rights reserved</b>
+    </footer>
 </div>
-<?php include 'includes/scripts.php' ?>
+<?php include 'includes/scripts.php'; ?>
 </body>
 </html>
